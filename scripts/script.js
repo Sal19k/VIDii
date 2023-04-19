40688cd978d580eab5c96ef8d1421abb26016483
Sal19k <90224227+Sal19k@users.noreply.github.com>
2023-04-19 20:20:24 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,9 @@
-// JavaScript Document
 console.log("Howdy!");
+
+const button = document.getElementById("button");
+const papier = document.querySelector(".papier");
+
+papier.addEventListener("click", () => {
+    papier.classList.add("transform");
+});
+
