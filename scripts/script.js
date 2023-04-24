fe4a7391ea6b18a96ef8625c5a0084c4986bdc79
Sal19k <90224227+Sal19k@users.noreply.github.com>
2023-04-24 09:49:45 +0200

    maandag

--- scripts/script.js
+++ scripts/script.js
@@ -3,37 +3,45 @@ console.log("Howdy!");
 const next = document.getElementById("next");
 const back = document.getElementById("back");
 const papier = document.querySelector(".papier");
-const panels = document.querySelectorAll("section:nth-of-type(1),section:nth-of-type(2),section:nth-of-type(3),section:nth-of-type(4)")
+const pagina1 = document.querySelector("section article:nth-of-type(1)")
+const pagina2 = document.querySelector("section article:nth-of-type(2)")
+const panels = document.querySelectorAll("section section")
 
 let currentPanelIndex = -1;
 
-papier.addEventListener("click", () => {
-    papier.classList.add("transform");
-});
 
 next.addEventListener("click", () => {
     // Remove the 'hover' class from the current panel if it's not the initial panel
     if (currentPanelIndex !== -1) {
         panels[currentPanelIndex].classList.remove("hover");
     }
+    console.log("currentPanelIndex =", currentPanelIndex);
+    pagina1.classList.remove("weg");
+    papier.classList.add("transform");
 
     // Increment the index and use the modulo operator to cycle through the panels
     currentPanelIndex = (currentPanelIndex + 1) % panels.length;
 
     // Add the 'hover' class to the new current panel
     panels[currentPanelIndex].classList.add("hover");
+
+    if (currentPanelIndex === 8) {
+        pagina1.classList.add("weg");
+        pagina2.classList.add("transform");
+    }
 });
 
 back.addEventListener("click", () => {
-    // Remove the 'hover' class from the current panel if it's not the initial panel
     if (currentPanelIndex !== -1) {
         panels[currentPanelIndex].classList.remove("hover");
     }
 
-    // Decrement the index and use the modulo operator to cycle through the panels
-    // Add panels.length before applying the modulo operator to ensure the index is always positive
     currentPanelIndex = (currentPanelIndex - 1 + panels.length) % panels.length;
+    panels[currentPanelIndex].classList.add("hover");   
 
-    // Add the 'hover' class to the new current panel
-    panels[currentPanelIndex].classList.add("hover");
+    if (currentPanelIndex === 7) {
+        pagina1.classList.add("weg");
+        papier.classList.remove("transform");
+        panels[currentPanelIndex].classList.remove("hover");
+    }
 });
